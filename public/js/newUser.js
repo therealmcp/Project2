361282f8a353cc6ee23316ff89eb1e0bdb16ffb7
PearsonJ <pearsonjoshua13131992@gmail.com>
2018-11-26 21:12:15 -0800

    added routes to connect signup page

--- public/js/newUser.js
+++ public/js/newUser.js
@@ -2,9 +2,15 @@ $("#add-btn").on("click", function(event) {
   event.preventDefault();
   // Make a newUser object
   var newUser = {
-    name: $("#userName").val().trim(),
-    email: $("#userEmail").val().trim(),
-    password: $("#userPassword").val().trim()
+    name: $("#userName")
+      .val()
+      .trim(),
+    email: $("#userEmail")
+      .val()
+      .trim(),
+    password: $("#userPassword")
+      .val()
+      .trim()
   };
 
   // Send an AJAX POST-request w jQuery
