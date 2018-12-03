022a7d3a69267eab81a2660c646e531ab4db534b
PearsonJ <pearsonjoshua13131992@gmail.com>
2018-12-03 14:53:46 -0800

    fixed sign in delay issue as well as got databases working again and added logout button

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define("User", {
+  var User = sequelize.define("users", {
     email: DataTypes.STRING,
     name: DataTypes.STRING,
     password: DataTypes.STRING,
