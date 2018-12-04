009934d849c93c9d108d32a3af91773915edf48a
therealmcp <cp.mcpike@gmail.com>
2018-12-03 19:30:18 -0800

    committing before pull. added badges

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -10,17 +10,14 @@ module.exports = function(app) {
   app.get("/profile", function(req, res) {
     var user = req.session.user;
     console.log(req.session);
-    db.Users
-      .findOne({
-        where: {
-          id: user.id
-        }
-      })
-      .then(function(user) {
-        res.render("profile", {
-          msg: "hey guys",
-          userData: user
-        });
+    db.Users.findOne({
+      where: {
+        id: user.id
+      }
+    }).then(function(user) {
+      res.render("profile", {
+        msg: "hey guys",
+        userData: user
       });
     });
   });
