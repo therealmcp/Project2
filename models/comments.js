2fc79bf984f2d265093f1295c82677ba0ab15111
PearsonJ <pearsonjoshua13131992@gmail.com>
2018-12-06 16:52:31 -0800

    posts now post in separate cards on both profile and on message page, Need to figure out why comments wont post on page and do a little more css clean up

--- models/comments.js
+++ models/comments.js
@@ -2,9 +2,17 @@ module.exports = function(sequelize, DataTypes) {
   var comments = sequelize.define("comments", {
     user: DataTypes.STRING,
     name: DataTypes.STRING,
-    post: DataTypes.STRING,
+    comments: DataTypes.STRING,
     pic: DataTypes.STRING,
     badge: DataTypes.STRING
   });
+
+  comments.associate = function(models) {
+    comments.belongsTo(models.posts, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
   return comments;
 };
